1aac80206f41b99634c46e6a58138849eab637c3
garyho7043 <72981943+garyho7043@users.noreply.github.com>
2022-09-28 03:22:37 +0800

    Add files via upload

---
+++ matrix_practice.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+A_2d = [[4,6,14],[6,14,36],[14,36,98]]
+B_2d =  []
+c = [[9],[16],[40]]
+B_2d = np.linalg.inv(A_2d)
+matmul_2D = np.matmul(B_2d, c)
+
+print("A_2d: \n",c)
+print("B_2d: \n",B_2d)
+
+print("\nA_2d*B_2d = \n",matmul_2D)
+
+
+
